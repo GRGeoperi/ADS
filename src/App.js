3b37a0bc5cc8113bd0a855d1d13b117815a8ea81
GRGeoperi <ggonzalezr2100@alumno.ipn.mx>
2025-01-12 19:15:28 -0600

    Eliminado el footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import SignUp from './components/sign-up/SignUp';
 import SignIn from './components/sign-in/SignIn';
-import Footer from './components/footer/Footer';
 import NavBar from './components/navbar/NavBar';
 
 function App() {
